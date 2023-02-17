1e60a42bd9b1fd550edf14018a7a513986f1bfc4
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-02-17 09:16:09 -0300

    Criando projeto para exemplo de tipos variaveis

--- Estudos/DIO/Bootcamps/Banco PAN/Sintaxe de Java/Dio trilha java basico/Tipos-Variaveis/src/TiposVariaveis.java
+++ Estudos/DIO/Bootcamps/Banco PAN/Sintaxe de Java/Dio trilha java basico/Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // estudem tipos primitivo
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Alexsander Vilar";
+
+        double salarioMinimo2023 = 1302; 
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        
     }
 }
