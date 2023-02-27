73a0c509532f26b0f7354c87efa65bfd83ff221a
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-02-27 19:14:50 -0300

    Estruturas de Repeticao e Arrays em Java

---
+++ DIO/Bootcamps/Banco PAN/Estruturas de Repeticao e Arrays em Java/Exercicios loops e arrays/src/EX5_Tabuada.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EX5_Tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de " + tabuada);
+
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# **Estudos**
-
-## Local onde irei organizar meus estudos e exercicios de acordo com as instituições, cursos e materias.
