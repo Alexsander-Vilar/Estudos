a0b6e31b6039aeb8e554f0413ad6d3468ff87e06
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-06-10 11:23:32 -0300

    Aprimoramento

--- Estudos/DIO/Bootcamps/Banco PAN/Poo/ex1/RodarAplicacao.java
+++ Estudos/DIO/Bootcamps/Banco PAN/Poo/ex1/RodarAplicacao.java
@@ -1,4 +1,4 @@
-package Poo.EX1;
+package Poo.ex1;
 
 public class RodarAplicacao {
 
@@ -10,17 +10,14 @@ public class RodarAplicacao {
         carro1.setModelo("BMW Série 3");
         carro1.setCapacidadeTanque(60);
 
-
         System.out.println(carro1.getCor());
         System.out.println(carro1.getModelo());
         System.out.println(carro1.getCapacidadeTanque());
-        System.out.println(carro1.totalValorTanque(6.85));
+        System.out.println("Valor para encher o tanque: R$" + carro1.totalValorTanque(6.85));
 
         Carro carro2 = new Carro("cinza", "Mercedes-Benz Classe C", 66);
 
-        System.out.println(carro2.getCor());
-        System.out.println(carro2.getModelo());
-        System.out.println(carro2.getCapacidadeTanque());
-        System.out.println(carro2.totalValorTanque(6.89));
+        System.out.println(carro2.toString());
+        System.out.println("Valor para encher o tanque: R$" + carro2.totalValorTanque(6.85));
     }
 }
